aa70da78c6be1a1b6ddc0dd129c68267f65092b8
Logen <lthornton1@mail.csuchico.edu>
2020-04-26 18:15:00 -0700

    added test cases for sortDescending and isPalindrome

--- Practice.cpp
+++ Practice.cpp
@@ -7,6 +7,12 @@ using std::string;
 // descending order from greatest (first) to least (third)
 void Practice::sortDescending(int & first, int & second, int & third)
 {
+  if( second < third )
+  {
+    int temp = second;
+    second = third;
+    third = temp;
+  }
   if( first < second )
   {
     int temp = first;
@@ -17,12 +23,6 @@ void Practice::sortDescending(int & first, int & second, int & third)
   {
     int temp = second;
     second = third;
-    third = temp;
-  }
-  if( first < third )
-  {
-    int temp = first;
-    first = third;
     third = temp;
   }
 }
--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,96 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+TEST(PracticeTest, is_one_letter_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a");
+    ASSERT_TRUE(actual);
+}
+
 TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_odd_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_mixed_case_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("ABCDdcba");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_special_char_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("!@#$%^&*()()*&^%$#@!");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_complex_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("ABCD%rAcEcAr%dcba");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_spaces_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("  ");
+    ASSERT_TRUE(actual);
+}
+
+
+TEST(PracticeTest, is_not_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("ab");
+    ASSERT_FALSE(actual);
+}
+
+
+TEST(PracticeTest, asc_sort_desc)
+{
+    Practice obj;
+		int one = 1, two = 2, three = 3;
+    bool actual = obj.sortDescending(one,two,three);
+    ASSERT_GE(one, two);
+		ASSERT_GE(two, three);
+}
+
+TEST(PracticeTest, same_sort_desc)
+{
+    Practice obj;
+		int one = 1, two = 1, three = 1;
+    bool actual = obj.sortDescending(one,two,three);
+    ASSERT_GE(one, two);
+		ASSERT_GE(two, three);
+}
+
+TEST(PracticeTest, desc_sort_desc)
+{
+    Practice obj;
+		int one = 3, two = 2, three = 1;
+    bool actual = obj.sortDescending(one,two,three);
+    ASSERT_GE(one, two);
+		ASSERT_GE(two, three);
+}
+
+TEST(PracticeTest, neg_sort_desc)
+{
+    Practice obj;
+		int one = -3, two = 2, three = 1;
+    bool actual = obj.sortDescending(one,two,three);
+    ASSERT_GE(one, two);
+		ASSERT_GE(two, three);
 }
